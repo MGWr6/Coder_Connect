57411c03978354709d2531edd7e3977f4cc08dd2
MLTurner <mlturne3@ncsu.edu>
2022-07-20 20:06:23 -0400

    created basic component files and structured router in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import './components/HomeFeed';
+import './components/NavBar';
+import './components/ProfilePage';
+import './components/error404'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter> 
+    <NavBar></NavBar>
+    <Switch>
+      <Route exact path="/" component={HomeFeed}></Route>
+
+    </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/HomeFeed.js
@@ -0,0 +1,11 @@
+const React = require("react");
+
+function HomeFeed()  {
+    return (
+        <main>
+        <img src="../public/HomeFeed.gif" alt="GIF welcoming user to home feed"/>
+        </main>
+    );
+}
+
+export default HomeFeed;
